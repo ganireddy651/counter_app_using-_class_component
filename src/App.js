4e1edf259629ffbec61f61363b9e344984b009f8
root <ganireddy651>
2023-05-21 11:37:18 +0000

    counter app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import Welcome from './components/Welcome'
+import Counter from './components/Counter'
+
+const App = () => {
+  return (
+    <div>
+      {/* <Welcome name="Ganesh" /> */}
+      <Counter />
+    </div>
+  )
+}
+export default App
